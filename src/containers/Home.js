5ff695ebd8118c7cf52e98885950271cc5124235
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-10-01 15:34:07 -0400

    incorporated state variables for weather info

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, {  useState, useEffect, useMemo  } from 'react';
+
+import axios from 'axios';
 
 import Header from '../components/Header';
 
 const weatherKey = `e8287c626eae68fcf8dcc499839c724e`;
 
 function Home() {
+    const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState('Tokyo');
+
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`
+            )
+            .then(function (response) {
+                // successful request
+                const weather = response.data;
+                setWeatherData(weather);
+            })
+            .catch(function (error) {
+                // error
+                console.log(error);
+            });
+    }, [city]);
+
+    const { 
+        cloudiness,
+        currentTemp, 
+        highTemp, 
+        humidity,
+        lowTemp, 
+        weatherType,
+        windSpeed,
+    } = useMemo(() => {
+        let cloudiness = '';
+        let currentTemp = '';
+        let highTemp = '';
+        let humidity = '';
+        let lowTemp = '';
+        let weatherType = '';
+        let windSpeed = '';
+
+        if(weatherData) {
+            cloudiness = weatherData.clouds.all + '%';
+            currentTemp = weatherData.main.temp
+            highTemp = weatherData.main.temp_max
+            humidity = weatherData.main.humidity + '%';
+            lowTemp = weatherData.main.temp_min
+            weatherType = weatherData.weather[0].description
+            windSpeed = weatherData.wind.speed + 'km/h';
+        }
+
+        return { 
+            cloudiness,
+            currentTemp,
+            highTemp,
+            humidity,
+            lowTemp,
+            weatherType,
+            windSpeed,
+         };
+    }, [weatherData]);
+
     // Display:
     // Weather Type (ex. Cloudy)
     // Current Temperature
@@ -14,19 +73,21 @@ function Home() {
     // Humidity
     // Wind Speed  
 
+    console.log("weatherData", weatherData);
+
     return (
         <div>
             <Header />
             <main className="Home">
-                <h2>Weather in Seoul</h2>
+                <h2>Weather in {city}</h2>
                 <div className="WeatherInfo">
-                    <p>Weather Type: Cloudy</p>
-                    <p>Current Temperature: 100 degrees</p>
-                    <p>High Temperature: 100 degrees</p>
-                    <p>Low Temperature: 80 degrees</p>
-                    <p>Cloudiness: 100</p>
-                    <p>Humidity: 35%</p>
-                    <p>Wind Speed: 3km/h</p>
+                    <p>Weather Type: {weatherType}</p>
+                    <p>Current Temperature: {currentTemp}</p>
+                    <p>High Temperature: {highTemp}</p>
+                    <p>Low Temperature: {lowTemp}</p>
+                    <p>Cloudiness: {cloudiness}</p>
+                    <p>Humidity: {humidity}</p>
+                    <p>Wind Speed: {windSpeed}</p>
                 </div>
             </main>
         </div>
